3b16d8f47474117ae1fe5b22b802814800cdc043
Arsalan Kazmi <arsalankazmi580@gmail.com>
2023-01-20 15:38:49 +0000

    Added a startup message
    I managed to get a randomness thing going! :D

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// main.rs - sued, the text editor of all time, short for Shut Up Editor
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+	let messages = vec!["the editor of all time", "shut up and edit", "the nonstandard text editor", "sued as in editor, not as in law"];
+	let message = messages[rand::thread_rng().gen_range(0..messages.len())];
+    println!("sued - {message}");
 }
