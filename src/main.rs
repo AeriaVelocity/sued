5e750e56b5d2b564ff0d5e254a62e774d8fc2c25
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-02-03 20:10:34 +0000

    open() function has been implemented
    Commands are now functions, too.
    Still no editor functionality. Rust is a bit different to other stuff I've used so yeah.
    show() function (~show command) has also been added, but I need to write a file buffer first.
    This will take a while.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,55 @@
 // main.rs - sued, the text editor of all time, short for Shut Up Editor
 
 use std::io;
+use std::fs;
 use rand::Rng;
 
-fn main() {
-    // Startup message
+fn startup_message() {
     let messages: Vec<&str> = vec!["the editor of all time", "shut up and edit", "the nonstandard text editor", "sued as in editor, not as in law"];
     let message: &str = messages[rand::thread_rng().gen_range(0..messages.len())];
-    println!("sued - {message}\n`~exit` or <C-c> exits");
+    println!("sued - {message}\ntype ~ for commands");
+}
 
-    // Create 'command' variable
+fn display_help() {
+    println!("~save, ~open, ~show, ~exit");
+}
+
+fn save() {
+    println!("sorry, can't save yet");
+}
+
+fn show() {
+    println!("sorry, no file buffer yet");
+}
+
+fn open(command_args: Vec<&str>) -> &str {
+    if command_args.len() <= 1 {
+        println!("open what?");
+        return "";
+    }
+    else {
+        return command_args[1];
+    }
+}
+
+fn main() {
+    startup_message();
     let mut command: String = String::new();
-
-    // Main while loop
     while !command.eq("~exit") {
-        // Clear the command variable, since read_line appends
         command.clear();
-
-        // Read user input
         io::stdin()
             .read_line(&mut command)
             .expect("Failed to read command.");
-
-        // Remove newline from the end of the command variable,
-        // since read_line adds a newline which makes sense actually
         let len: usize = command.trim_end_matches(&['\r', '\n'][..]).len();
         command.truncate(len);
-
-        // Handle commands, which don't yet do anything
-        // If a command isn't recognised, the text will
-        // be println!'d instead
+        let command_args = command.split(" ").collect::<Vec<&str>>();
         // TODO Will replace with editor functionality
-        let _cmdproc: () = match command.as_str() {
-            "~save" => { println!("not implemented"); },
-            "~open" => { println!("not implemented"); },
-            _ => { println!("{}", command) }
+        let _cmdproc: () = match command_args[0] {
+            "~"     => { display_help(); },
+            "~save" => { save(); },
+            "~show" => { show(); },
+            "~open" => { println!("{}", open(command_args)); },
+            _       => { println!("{}", command); }
         };
     }
 }
