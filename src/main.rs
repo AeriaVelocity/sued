f0734d0256ef994d1e2ef24a75a504b4d7a570c8
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-03-08 18:10:18 +0000

    More startup messages and new function
    You can now run external program with ~run [program], like how ![program] works in ed.
    I probably should have written the actual text editor functionality first. :p

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,42 @@
 
 use std::io;
 use std::fs;
+use std::process::Command;
+use which::which;
 use rand::Rng;
 
 fn startup_message() {
-    let messages: Vec<&str> = vec!["the editor of all time", "shut up and edit", "the nonstandard text editor", "sued as in editor, not as in law"];
+    let messages: Vec<&str> = vec![
+        "the editor of all time",
+        "shut up and edit",
+        "the nonstandard text editor",
+        "sued as in editor, not as in law",
+        "sued, man! ~run man sued",
+        "there is no visual mode",
+        "the itor fell off",
+    ];
     let message: &str = messages[rand::thread_rng().gen_range(0..messages.len())];
-    println!("sued - {message}\ntype ~ for commands");
+    println!("sued - {message}\ntype ~ for commands, otherwise just start typing");
 }
 
 fn display_help() {
-    println!("~save, ~open, ~show, ~exit");
+    println!("~save, ~open, ~show, ~run, ~exit, ~help");
+}
+
+fn extended_help() {
+    println!("sued is a line editor, like ed but also not at all\n\
+              to write stuff, just start typing after the welcome message\n\
+              editor commands are prefixed with ~ (for example ~exit to quit the editor)\n\
+              there's no regex stuff or syntax highlighting or anything like that. you just write\n\
+              sued written by Arsalan Kazmi (That1M8Head)");
 }
 
 fn save() {
-    println!("sorry, can't save yet");
+    println!("sorry, can't save yet"); // TODO write the file buffer first. then implement saving
 }
 
 fn show() {
-    println!("sorry, no file buffer yet");
+    println!("sorry, no file buffer yet"); // TODO write the damn file buffer already
 }
 
 fn open(command_args: Vec<&str>) -> &str {
@@ -32,6 +50,34 @@ fn open(command_args: Vec<&str>) -> &str {
     }
 }
 
+fn write(to_write: &str) {
+    // TODO implement file buffer, then implement writing to buffer
+    println!("{}", to_write);
+}
+
+fn shell_command(mut command_args: Vec<&str>) {
+    if command_args.len() <= 1 {
+        println!("run what?");
+    }
+    else {
+        let cmdpath = which(command_args[1]);
+        let command = command_args[1];
+        match which(command) {
+            Ok(_) => println!("running {}", command),
+            Err(_) => {
+                println!("no such command");
+                return;
+            }
+        }
+        command_args.drain(0..2);
+        Command::new(cmdpath.clone().unwrap())
+            .args(command_args.clone())
+            .status()
+            .expect("broken!");
+        println!("finished running {}", command);
+    }
+}
+
 fn main() {
     startup_message();
     let mut command: String = String::new();
@@ -39,17 +85,20 @@ fn main() {
         command.clear();
         io::stdin()
             .read_line(&mut command)
-            .expect("Failed to read command.");
+            .expect("can't read command");
         let len: usize = command.trim_end_matches(&['\r', '\n'][..]).len();
         command.truncate(len);
         let command_args = command.split(" ").collect::<Vec<&str>>();
-        // TODO Will replace with editor functionality
+        // TODO replace with editor functionality
         let _cmdproc: () = match command_args[0] {
             "~"     => { display_help(); },
+            "~help" => { extended_help(); },
             "~save" => { save(); },
             "~show" => { show(); },
             "~open" => { println!("{}", open(command_args)); },
-            _       => { println!("{}", command); }
+            "~run"  => { shell_command(command_args); },
+            "~exit" => {},
+            _       => { if command_args[0].starts_with("~") { println!("{} is an unknown command", command_args[0]); } else { write(command.as_str()); } }
         };
     }
 }
